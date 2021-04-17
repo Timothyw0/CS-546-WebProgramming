2c9102d0a54fbe9834d313359a86374c5a8b7aa6
ppondo <patrickpondo7@gmail.com>
2021-04-17 14:49:40 -0400

    add date and _id validation

--- Code/data/validation.js
+++ Code/data/validation.js
@@ -1,5 +1,5 @@
 const emailValidator = require('email-validator');
-
+const { ObjectId } = require('mongodb')
 function validString(string) {
     if(typeof string !== 'string' || !string) return false;
 
@@ -10,3 +10,28 @@ function validEmail(email) {
     return emailValidator.validate(email);
 }
 
+function validDate(date) {
+    checkString(date);
+    let splitDate = date.split('/')
+
+    if (splitDate[0].length > 2 || splitDate[0].length < 1) return false;
+    if (splitDate[1].length > 2 || splitDate[0].length < 1) return false;
+    if (splitDate[2].length !== 4) return false;
+
+    let dateObj = new Date(date);
+    if (!dateObj) return false
+
+    return true;
+}
+
+function validId(id) {
+    if (!ObjectId(id) || !validString(id)) return false;
+    return true;
+}
+
+module.exports = {
+    validString,
+    validEmail,
+    validDate,
+    validId
+}
