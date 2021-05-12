a6a94f0fd9715b8fa2bc38c34e3fc0bc6927778b
ppondo <patrickpondo7@gmail.com>
2021-05-12 14:26:44 -0400

    style and adjust recipe search handlebar

--- Code/data/search.js
+++ Code/data/search.js
@@ -2,14 +2,18 @@ const mongoCollections = require('../config/mongoCollections');
 const recipes = mongoCollections.recipes;
 const validation = require('./validation');
 const { ObjectId } = require('mongodb');
+const { getAllRecipes } = require('./recipes');
 
 async function getRecipeByAlc (alcoholName) {
     if (!validation.validString(alcoholName)) throw 'Invalid alcohol name';
     let cleanAlc = alcoholName.trim().toLowerCase();
     const recipeCollection = await recipes();
-    const recipesList = await recipeCollection.find({alcohol: cleanAlc}).toArray();
+    
+    const recipesList = await recipeCollection.find(
+        { alcohol: cleanAlc }
+    ).toArray();
 
-    if(recipesList.length < 1 || recipesList !== null) throw 'No recipes found'
+    if(recipesList.length < 1 || recipesList == null) throw 'No recipes found'
 
     for (let recipe of recipesList) {
         recipe._id = recipe._id.toString();
