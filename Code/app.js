9ec7bd01b809a9eed93f3095d99c08bccfbeb8f1
Timothyw0 <timothyw0@gmail.com>
2021-05-14 09:27:35 -0400

    Added middleware to link My Profile tab

--- Code/app.js
+++ Code/app.js
@@ -5,7 +5,7 @@ const static = express.static(__dirname + "/public");
 const configRoutes = require("./routes");
 const exphbs = require("express-handlebars");
 const Handlebars = require("handlebars");
-var fs = require('fs');
+var fs = require("fs");
 
 const handlebarsInstance = exphbs.create({
   defaultLayout: "main",
@@ -36,17 +36,15 @@ app.use(express.urlencoded({ extended: true }));
 app.engine("handlebars", handlebarsInstance.engine);
 app.set("view engine", "handlebars");
 
-
-
-
 app.use(async (req, res, next) => {
-  var timestamp = new Date().toUTCString()
+  var timestamp = new Date().toUTCString();
   //console.log(`[${timestamp}] ${req.method} ${req.originalUrl}`);
-  
-  fs.appendFile('./log.txt',
-  `[${timestamp}] ${req.method} ${req.originalUrl} \r\n`
-  , () => {
-  })
+
+  fs.appendFile(
+    "./log.txt",
+    `[${timestamp}] ${req.method} ${req.originalUrl} \r\n`,
+    () => {}
+  );
   next();
 });
 
@@ -63,11 +61,30 @@ app.use("/feed", (req, res, next) => {
   if (!req.session.user) {
     return res.redirect("/");
   } else {
+    // When user goes to feed and is fully logged in, display My Profile tab in navbar
+    const userID = req.session.user._id;
+    app.locals.loggedIn = true;
+    app.locals.myID = userID;
     next();
   }
 });
 
+// When user logs out, clear the My Profile tab in navbar
+app.use("/logout", (req, res, next) => {
+  app.locals.loggedIn = false;
+  app.locals.myID = null;
+  next();
+});
+
 app.use("/posts", (req, res, next) => {
+  if (!req.session.user) {
+    return res.redirect("/");
+  } else {
+    next();
+  }
+});
+
+app.use("/recipes", (req, res, next) => {
   if (!req.session.user) {
     return res.redirect("/");
   } else {
