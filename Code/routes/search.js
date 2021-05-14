b30425b5a66ef1c0fb3f9e18a9edaeea7fcb146d
Billy Kingsbery <34725805+wkingsbery@users.noreply.github.com>
2021-05-14 13:00:00 -0400

    Update search.js
    Added user routes

--- Code/routes/search.js
+++ Code/routes/search.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const xss = require("xss");
 const searchData = require("../data/search");
+const userData = require("../data/userData")
 const validator = require('../data/validation');
 
 router.get('/recipe', async (req, res) => { 
@@ -34,6 +35,42 @@ router.post('/recipe', async (req, res) => {
     } catch (e) {
         errors.push(e);
         return res.status(404).render('search/recipes', {
+            title: `Results for ${req.body.alcohol}`,
+            searchTerm: req.body.alcohol,
+            errors: errors 
+        });
+    }
+});
+
+router.get('/user', async (req, res) => { 
+    if(!req.session.user) {
+        res.redirect('/');
+    } else {
+        res.render('search/user', {title: 'User Search'});
+    }
+});
+
+router.post('/user', async (req, res) => { 
+    let errors = [];
+    if(!validator.validString(req.body.searchUsername)) errors.push('No User Profile Found')
+    const usernameToSearch = xss(req.body.searchUsername.toLowerCase().trim());
+    console.log(usernameToSearch)
+    if (errors.length > 0) {
+        return res.render('search/user', {
+            title: 'User Search',
+            searchTerm: req.body.searchUsername,
+            errors: errors 
+        });
+    }
+
+    try {
+        const userFound = await userData.findUserByUsername(usernameToSearch);
+        const userID = userFound._id;
+        console.log(userID)
+        res.redirect(`/users/profile/${userID}`);
+    } catch (e) {
+        errors.push(e);
+        return res.status(404).render('search/user', {
             title: `Results for ${req.body.alcohol}`,
             searchTerm: req.body.alcohol,
             errors: errors 
